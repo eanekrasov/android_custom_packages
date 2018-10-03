ff70c4cacd9fcfdef7a6555e5cbdbf32f1af0490
coder <coder@1dea.ru>
2018-10-03 17:31:46 +0300

    Update FoilGuard/Android.mk, FoilGuard/foilguard-release.apk, FoilGuard/privapp-permissions-ru.offenso.foilguard.xml files

---
+++ FoilGuard/Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := privapp-permissions-ru.offenso.foilguard.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := FoilGuard
+LOCAL_SRC_FILES := foilguard-release.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_REQUIRED_MODULES := privapp-permissions-ru.offenso.foilguard.xml
+include $(BUILD_PREBUILT)
