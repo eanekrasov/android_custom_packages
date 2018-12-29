8c578f8d8f0e1083bc6b9367510eca8c183db2ff
coder <coder@1dea.ru>
2018-12-29 16:16:22 +0300

    trying to fix issue with privileged extension

--- FDroidPrivilegedExtension/Android.mk
+++ FDroidPrivilegedExtension/Android.mk
@@ -1,4 +1,5 @@
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := privapp-permissions-org.fdroid.fdroid.privileged.xml
 LOCAL_MODULE_TAGS := optional
@@ -6,10 +7,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := FDroidPrivilegedExtension
-LOCAL_SRC_FILES := fdroid-privileged-extension-release.apk
+LOCAL_SRC_FILES := org.fdroid.fdroid.privileged_2080.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
